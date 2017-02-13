74cad281c1b890744b171794e5f743854dbb8a12
Marcel Schmittfull <mschmittfull@gmail.com>
2017-02-13 15:31:57 -0500

    added install script for perseus cluster at princeton

---
+++ install_perseus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install dependencies:
+#pip install --target=/home/mschmittfull_ias.edu/software/python/ cython
+#pip install --target=/home/mschmittfull_ias.edu/software/python/ -r requirements.txt
+
+#Installation at IAS
+pip install -U --force --target=/home/mschmittfull_ias.edu/software/python/ --no-deps .
